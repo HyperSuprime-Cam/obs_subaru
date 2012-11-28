204c47b00d6d2f3f23d2d823fb68cbbc264bf6a4
Jim Bosch <jbosch@astro.princeton.edu>
2012-11-28 13:01:10 -0500

    add additional aperture in forced photometry

--- config/forcedPhot.py
+++ config/forcedPhot.py
@@ -7,6 +7,16 @@ root.references.retarget(SubaruReferencesTask)
 # Measurement
 root.measurement.algorithms["flux.gaussian"].shiftmax = 10.0
 
+# Add another aperture flux - it shouldn't be this painful to do (see LSST #2465)
+from lsst.meas.algorithms.algorithmRegistry import AlgorithmRegistry, SincFluxConfig
+AlgorithmRegistry.register("flux.sinc2", target=SincFluxConfig.Control, ConfigClass=SincFluxConfig)
+root.measurement.algorithms["flux.sinc2"].radius = 5.0
+root.measurement.algorithms.names |= ["flux.sinc2"]
+
+# Copy deblending flags
+root.copyColumns["deblend.nchild"] = "deblend.nchild"
+root.copyColumns["parent"] = "parentObjectId"
+
 # Enable model mags
 try:
     import lsst.meas.extensions.multiShapelet
@@ -14,3 +24,4 @@ try:
     root.measurement.slots.modelFlux = "multishapelet.combo.flux"
 except ImportError:
     print "meas_extensions_multiShapelet is not setup; disabling model mags"
+
