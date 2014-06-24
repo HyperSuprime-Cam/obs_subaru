eaad45840ea23d3240207d42f7b522d150c06b5a
Jim Bosch <jbosch@astro.princeton.edu>
2014-06-24 15:18:50 -0400

    Set default aperture for flux.sinc to 12pix radius (may need to increase even further in future)

--- config/processCcd.py
+++ config/processCcd.py
@@ -56,6 +56,12 @@ root.measurement.algorithms.names |= ["flux.aperture"]
 # Roughly (1.0, 1.4, 2.0, 2.8, 4.0, 5.7, 8.0, 11.3, 16.0, 22.6 arcsec) in diameter: 2**(0.5*i)
 root.measurement.algorithms["flux.aperture"].radii = [3.0, 4.5, 6.0, 9.0, 12.0, 17.0, 25.0, 35.0, 50.0, 70.0]
 
+# Use a large aperture to be independent of seeing in calibration
+root.calibrate.measurement.algorithms["flux.sinc"].radius = 12.0
+
+# Use a large aperture to be independent of seeing in calibration
+root.measurement.algorithms["flux.sinc"].radius = 12.0
+
 try:
     import lsst.meas.extensions.photometryKron
     root.measurement.algorithms.names |= ["flux.kron"]
