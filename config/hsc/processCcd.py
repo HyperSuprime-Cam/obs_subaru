d7f24803b1e5f3011226980190faa63449353936
Naoki Yasuda <yasudank@gmail.com>
2014-07-18 14:34:30 +0900

    Change starSelector['objectSize'].widthMin to smaller value to avoid a failure for good seeing data.

--- config/hsc/processCcd.py
+++ config/hsc/processCcd.py
@@ -9,7 +9,7 @@ root.calibrate.photocal.colorterms.load(os.path.join(os.environ['OBS_SUBARU_DIR'
                                                      'colorterms.py'))
 
 root.calibrate.measurePsf.starSelector.name='objectSize'
-root.calibrate.measurePsf.starSelector['objectSize'].widthMin=1.0
+root.calibrate.measurePsf.starSelector['objectSize'].widthMin=0.9
 
 root.calibrate.astrometry.solver.sipOrder = 3
 root.calibrate.astrometry.solver.catalogMatchDist = 2.0
