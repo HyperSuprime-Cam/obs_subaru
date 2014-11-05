e3012f5d4a36d4179546de6582a7411565ee69dc
Bob Armstrong <rearmstr@gmail.com>
2014-11-05 12:27:41 -0500

    Add code to build validPolygon for each image.
    The settings for the vignetted region was broken out into a
    separate config file to be used in different places in the code.

--- config/hsc/flat.py
+++ config/hsc/flat.py
@@ -4,6 +4,4 @@ root.isr.doFringe=False
 
 from lsst.obs.hsc.detrends import HscFlatCombineTask
 root.combination.retarget(HscFlatCombineTask)
-root.combination.xCenter = -100
-root.combination.yCenter = 100
-root.combination.radius = 17500
+root.combination.load(os.path.join(os.environ['OBS_SUBARU_DIR'], 'config', 'hsc', 'vignette.py'))
---
+++ config/hsc/vignette.py
@@ -0,0 +1,4 @@
+
+root.xCenter = -100
+root.yCenter = 100
+root.radius = 17500
