d400834c156347059134b29bf4de948b2ae4e4bb
Paul Price <price@astro.princeton.edu>
2015-02-27 10:43:00 -0500

    config: adapt to use of detectCoaddSources in stack.py
    The super-stacker has been adapted to use the DetectCoaddSourcesTask
    instead of a straight SourceDetectionTask.  Nevertheless, we want to
    be able to use the same config override file as for processCoadd.py,
    so we use a try/except block to select the scenario.

--- config/hsc/processCoadd.py
+++ config/hsc/processCoadd.py
@@ -3,7 +3,12 @@ HSC-specific overrides for ProcessCoaddTask
 (applied after Subaru overrides in ../processCoadd.py).
 """
 
-root.detection.background.useApprox = False
-root.detection.background.binSize = 4096
-root.detection.background.undersampleStyle = 'REDUCE_INTERP_ORDER'
+try:
+    detection = root.detectCoaddSources.detection
+except:
+    detection = root.detection
+
+detection.background.useApprox = False
+detection.background.binSize = 4096
+detection.background.undersampleStyle = 'REDUCE_INTERP_ORDER'
 
