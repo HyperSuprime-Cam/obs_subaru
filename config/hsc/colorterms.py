f908cffcbc61b2f842b30c4821ee29ea2160538d
Naoki Yasuda <yasudank@gmail.com>
2015-03-02 12:13:44 +0900

    Add color terms for NB921 and NB816.

--- config/hsc/colorterms.py
+++ config/hsc/colorterms.py
@@ -16,6 +16,8 @@ root.library = {
          'i': ColortermConfig.fromValues("i", "z",  0.00130204, -0.16922042, -0.01374245),
          'z': ColortermConfig.fromValues("z", "i", -0.00680620,  0.01353969,  0.01479369),
          'y': ColortermConfig.fromValues("z", "i",  0.01739708,  0.35652971,  0.00574408),
+         'NB816': ColortermConfig.fromValues("i", "z",  0.00927133, -0.63558358, -0.05474862),
+         'NB921': ColortermConfig.fromValues("z", "i",  0.00752972,  0.09863530, -0.05451118),
          }),
     "ps1*": ColortermGroupConfig.fromValues(
         {'g': ColortermConfig.fromValues("g", "r",  0.00730066,  0.06508481, -0.01510570),
@@ -23,5 +25,7 @@ root.library = {
          'i': ColortermConfig.fromValues("i", "z",  0.00166891, -0.13944659, -0.03034094),
          'z': ColortermConfig.fromValues("z", "y", -0.00907517, -0.28840221, -0.00316369),
          'y': ColortermConfig.fromValues("y", "z", -0.00156858,  0.14747401,  0.02880125),
+         'NB816': ColortermConfig.fromValues("i", "z",  0.01191062, -0.68757034, -0.10781564),
+         'NB921': ColortermConfig.fromValues("z", "y",  0.00142051, -0.59278367, -0.25059679),
          }),
 }
