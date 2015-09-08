1abea6ba0835278faed8bbbe25b8f6b19191a90e
Paul Price <price@astro.princeton.edu>
2015-09-08 15:47:43 -0400

    config: remove processCoadd from stack.py
    We still use detectCoaddSources within stack.py, so it essentially
    replaces processCoadd.

---
+++ config/hsc/detectCoaddSources.py
@@ -0,0 +1,4 @@
+# Overrides for detectCoaddSources.py
+root.detection.background.useApprox = False
+root.detection.background.binSize = 4096
+root.detection.background.undersampleStyle = 'REDUCE_INTERP_ORDER'
