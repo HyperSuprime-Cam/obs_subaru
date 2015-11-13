0392b4e900ec6dc8bcd83e57fb42a50dc0186f56
Paul Price <price@astro.princeton.edu>
2015-11-13 10:11:33 -0500

    config: fix cut/paste error
    The 'root.' was missing.

--- config/hsc/processCoadd.py
+++ config/hsc/processCoadd.py
@@ -3,9 +3,9 @@ HSC-specific overrides for ProcessCoaddTask
 (applied after Subaru overrides in ../processCoadd.py).
 """
 
-detection.background.useApprox = False
-detection.background.binSize = 4096
-detection.background.undersampleStyle = 'REDUCE_INTERP_ORDER'
+root.detection.background.useApprox = False
+root.detection.background.binSize = 4096
+root.detection.background.undersampleStyle = 'REDUCE_INTERP_ORDER'
 
 import os
 root.astrometry.solver.load(os.path.join(os.environ["OBS_SUBARU_DIR"], "config", "hsc", "filterMap.py"))
